ee6f759ab7b1fa34a5ec24a98480d749144dcf82
elite <elite@MacBook-Air-de-Admin0001.local>
2023-12-26 14:44:54 +0100

    last

--- main.c
+++ main.c
@@ -1,12 +1,10 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
-#include <arpa/inet.h>
 #include <sys/socket.h>
 #include <netinet/in.h>
 #include <string.h>
 #include <errno.h>
-#include <pthread.h>
 #include "job.h"
 
 #define MAX_CLIENTS 5
@@ -18,21 +16,25 @@ int main() {
     int opt = 1;
     socklen_t addrlen = sizeof(address);
 
+    // Create a socket
     if ((server_fd = socket(AF_INET, SOCK_STREAM, 0)) == 0) {
         perror("Échec de la création du socket serveur");
         exit(EXIT_FAILURE);
     }
 
+    // Set socket options
     if (setsockopt(server_fd, SOL_SOCKET, SO_REUSEADDR, &opt, sizeof(opt))) {
         perror("setsockopt");
         exit(EXIT_FAILURE);
     }
 
+    // Bind the socket to a specific IP address and port
     address.sin_family = AF_INET;
     address.sin_addr.s_addr = INADDR_ANY;
     address.sin_port = htons(PRINTERPORT);
 
-    if (bind(server_fd, (struct sockaddr *)&address, sizeof(address)) < 0) {
+    // Start listening for incoming connections
+    if (bind(server_fd, (struct sockaddr *) &address, sizeof(address)) < 0) {
         perror("Échec de la liaison du socket serveur");
         exit(EXIT_FAILURE);
     }
@@ -42,19 +44,22 @@ int main() {
         exit(EXIT_FAILURE);
     }
 
+    // Loop to accept incoming connections
     while (1) {
-        if ((new_socket = accept(server_fd, (struct sockaddr *)&address, &addrlen)) < 0) {
+        if ((new_socket = accept(server_fd, (struct sockaddr *) &address, &addrlen)) < 0) {
             perror("Échec de l'acceptation de la connexion");
             exit(EXIT_FAILURE);
         }
 
+        // Buffer to store received data
         struct job buffer;
         memset(&buffer, 0, sizeof(struct job));
 
-        while(1) {
+        // Loop to receive data from the client
+        while (1) {
             int bytes_received = recv(new_socket, &buffer, sizeof(struct job), 0);
             if (bytes_received < 0) {
-                if(errno == EBADF) {
+                if (errno == EBADF) {
                     perror("Socket is not valid");
                 } else {
                     perror("Échec de la réception des données");
@@ -62,33 +67,38 @@ int main() {
                 exit(EXIT_FAILURE);
             }
 
-            if(bytes_received == 0) {
+            // If received data size is zero, it means the client has closed the connection
+            if (bytes_received == 0) {
                 printf("connection closed by client");
-                break; // Connection closed by client
+                close(new_socket); // Close the socket when done
+                break; // Exit the outer while loop
             }
 
+            // Process the received data
             FILE *printerFile;
             int ppm = 60;
             char name[20];
             sprintf(name, "imprimante_%s.log", buffer.printerId);
-            printf("%s \n",name);
+            printf("%s \n", name);
 
-            printerFile = fopen( name, "a" );
-            if ( printerFile == NULL ) {
-                perror( "Cannot open file\n" );
+            printerFile = fopen(name, "a");
+            if (printerFile == NULL) {
+                perror("Cannot open file\n");
                 exit(3);
             }
 
-            fprintf( printerFile, "Imprime %d pages : ", buffer.pages);
+            fprintf(printerFile, "Imprime %d pages : ", buffer.pages);
             fflush(printerFile);
 
-            sleep(buffer.pages * 60/ppm) ;
-            fprintf( printerFile, "Fait en %d secondes\n", buffer.pages *60/ppm );
+            sleep(buffer.pages * 60 / ppm);
+            fprintf(printerFile, "Fait en %d secondes\n", buffer.pages * 60 / ppm);
             fflush(printerFile);
-            printf("Fin impression %d \n", buffer.pages) ;
-            fflush(stdout) ;
-            printf("Received: Printer ID: %s, Pages: %d, Priority: %d \n", buffer.printerId, buffer.pages, buffer.priority);
-
+            printf("Fin impression %d \n", buffer.pages);
+            fflush(stdout);
+            printf("Received: Printer ID: %s, Pages: %d, Priority: %d \n", buffer.printerId, buffer.pages,
+                   buffer.priority);
+            close(new_socket); // Close the socket when done
+            break; // Exit the outer while loop
         }
     }
 
